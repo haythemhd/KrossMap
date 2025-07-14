909e60e055e7186b5966de321474aaa7c9472908
farimarwat <farimarwat@gmail.com>
2025-07-14 12:16:40 +0500

    Add kdoc

--- krossmap/src/commonMain/kotlin/com/farimarwat/krossmap/core/KrossCameraPositionState.kt
+++ krossmap/src/commonMain/kotlin/com/farimarwat/krossmap/core/KrossCameraPositionState.kt
@@ -3,21 +3,47 @@ package com.farimarwat.krossmap.core
 import androidx.compose.runtime.Composable
 import com.farimarwat.krossmap.model.KrossCoordinate
 
+/**
+ * Represents the camera state for the map, including the current position and camera controls.
+ */
 expect class KrossCameraPositionState {
 
+    /**
+     * The current position of the camera on the map.
+     */
     var currentCameraPosition: KrossCoordinate?
-    // Methods
 
+    /**
+     * Animates the camera to the given latitude and longitude with an optional bearing.
+     *
+     * @param latitude Target latitude.
+     * @param longitude Target longitude.
+     * @param bearing Direction the camera should face, in degrees. Default is 0.
+     */
     suspend fun animateCamera(latitude: Double, longitude: Double, bearing: Float = 0f)
 
+    /**
+     * Changes the tilt angle of the camera.
+     *
+     * @param tilt The new tilt angle in degrees.
+     */
     suspend fun changeTilt(tilt: Float)
 }
 
+/**
+ * Remembers and provides a [KrossCameraPositionState] for controlling and tracking the map camera.
+ *
+ * @param latitude Initial latitude position.
+ * @param longitude Initial longitude position.
+ * @param zoom Initial zoom level.
+ * @param tilt Initial tilt angle of the camera.
+ * @param bearing Initial bearing (direction) the camera should face.
+ */
 @Composable
-expect fun  rememberKrossCameraPositionState(
+expect fun rememberKrossCameraPositionState(
     latitude: Double,
     longitude: Double,
     zoom: Float,
     tilt: Float,
     bearing: Float
-):KrossCameraPositionState
+): KrossCameraPositionState
--- krossmap/src/commonMain/kotlin/com/farimarwat/krossmap/core/KrossMapState.kt
+++ krossmap/src/commonMain/kotlin/com/farimarwat/krossmap/core/KrossMapState.kt
@@ -6,27 +6,88 @@ import com.farimarwat.krossmap.model.KrossCoordinate
 import com.farimarwat.krossmap.model.KrossMarker
 import com.farimarwat.krossmap.model.KrossPolyLine
 
-expect class KrossMapState{
+/**
+ * Maintains the state of the KrossMap, including current location, markers, polylines,
+ * and location update controls.
+ */
+expect class KrossMapState {
+
+     /**
+      * The current location of the device, if available.
+      */
      var currentLocation: KrossCoordinate?
+
+     /**
+      * List of markers currently shown on the map.
+      */
      internal val markers: SnapshotStateList<KrossMarker>
+
+     /**
+      * List of polylines currently drawn on the map.
+      */
      internal val polylines: SnapshotStateList<KrossPolyLine>
 
+     /**
+      * Indicates whether a location request has been made.
+      */
      internal var currentLocationRequested: Boolean
+
+     /**
+      * The previous location coordinate before the current one.
+      */
      internal var previousCoordinates: KrossCoordinate?
 
-     var onUpdateLocation:(KrossCoordinate)-> Unit
+     /**
+      * Callback invoked whenever the location updates.
+      */
+     var onUpdateLocation: (KrossCoordinate) -> Unit
+
+     /**
+      * Adds or updates a marker on the map.
+      *
+      * @param marker The marker to add or update.
+      */
      fun addOrUpdateMarker(marker: KrossMarker)
+
+     /**
+      * Removes the given marker from the map.
+      *
+      * @param marker The marker to remove.
+      */
      fun removeMarker(marker: KrossMarker)
 
+     /**
+      * Adds a polyline to the map.
+      *
+      * @param polyLine The polyline to add.
+      */
      fun addPolyLine(polyLine: KrossPolyLine)
+
+     /**
+      * Removes the given polyline from the map.
+      *
+      * @param polyline The polyline to remove.
+      */
      fun removePolyLine(polyline: KrossPolyLine)
 
+     /**
+      * Requests the current location of the user. Note: It starts and stops location updates automatically
+      */
      fun requestCurrentLocation()
 
+     /**
+      * Starts continuous location updates.
+      */
      fun startLocationUpdate()
-     fun stopLocationUpdate()
 
+     /**
+      * Stops location updates.
+      */
+     fun stopLocationUpdate()
 }
 
+/**
+ * Remembers and provides a [KrossMapState] instance for managing map interaction and state.
+ */
 @Composable
 expect fun rememberKrossMapState(): KrossMapState
